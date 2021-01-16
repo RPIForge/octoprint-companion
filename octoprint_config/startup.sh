cc81a43b6d6d62bc572967ff870a9c7fff6a59d2
Michael Honaker <mchonaker@gmail.com>
2021-01-16 05:01:10 +0000

    Reorganized Directoy Structure.
    This also moved some files over to the deployment repository

--- octoprint_config/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "getting key"
-
-KEY=$(cat /octoprint/octoprint/config.yaml | grep key| cut -d ":" -f2 | xargs)
-echo "OCTOPRINT_KEY=$KEY">/config/octoprint.env
-
-sleep 1000
-exec "/usr/local/bin/python /usr/local/bin/octoprint serve --iknowwhatimdoing --host 0.0.0.0 --port 5000 --basedir /octoprint/octoprint"
