ffa9828c8533c1d32427052252dd0d8587b5e6fa
Michael Honaker <honakm@rpi.edu>
2021-02-08 18:49:47 +0000

    Update website url settings

--- companion/utils/website.py
+++ companion/utils/website.py
@@ -16,8 +16,7 @@ import json
 
 #Class for syncing variables
 class website():
-    ip = None
-    port = None
+    url = None
     api_key = None
     
     #generic variables
@@ -28,18 +27,10 @@ class website():
         self.logger = self.variable.logger_class.logger
         self.logger.info("Initalizing Website Class")
         
-        ip_string = os.getenv('SITE_URL',"http://localhost")
-        ip = ip_string.strip()
-        try:
-            new_ip = socket.gethostbyname(ip)
-            self.ip = new_ip
-        except socket.gaierror:
-            self.logger.error("{} is not a valid ip or hostname for website".format(ip))
+        #get site url
+        url_string = os.getenv('SITE_URL',"http://localhost:8000")
+        self.url = url_string.strip()
 
-        #get list of port numbers
-        port_string = os.getenv('SITE_PORT',"8000")
-        self.port = port_string.strip()
-        
         
         #get the api key
         self.api_key = os.getenv('SITE_KEY',"test_key")
@@ -50,7 +41,7 @@ class website():
         
     #format url    
     def get_url(self):
-        return "{}:{}".format(self.ip, self.port)
+        return self.url
     
     def make_get_request(self,endpoint, dictionary):
         try:
