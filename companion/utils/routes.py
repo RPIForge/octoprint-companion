f95e4faa0f02b8f894298336a4523a01595016f0
Michael Honaker <mchonaker@gmail.com>
2021-03-31 12:14:02 -0400

    Update routes to be inlign new MTConnect version

--- companion/utils/routes.py
+++ companion/utils/routes.py
@@ -12,51 +12,30 @@ def health_check():
 
 @endpoints.route('/probe')
 def probe():
-    return Response(variable_instance.mtconnect.probe(), mimetype='text/xml')
+    response = variable_instance.mtconnect.probe()
+    return Response(response.get_xml(),status=response.get_status(), mimetype='text/xml')
 
 @endpoints.route('/current', defaults={'identifier': None})
 @endpoints.route('/<identifier>/current')
 def current(identifier):
     path = request.args.get('path', None)
-    at = request.args.get('at', None)
- 
-    try:
-        at = int(at)
-    except:
-        pass
+    at = request.args.get('at', None, type=int)
 
     if(identifier is not None):
         path = ".//*[@id='{}'] | .//*[@name='{}']".format(identifier,identifier)
 
-    response = Response(variable_instance.mtconnect.current(at, path), mimetype='text/xml')
-    return response
+    response = variable_instance.mtconnect.current(at, path)
+    return Response(response.get_xml(),status=response.get_status(), mimetype='text/xml')
 
 @endpoints.route('/sample', defaults={'identifier': None})
 @endpoints.route('/<identifier>/sample')
 def sample(identifier):
     path = request.args.get('path', None)
-    start = request.args.get('from', None)
-    count = request.args.get('count',None)
+    start = request.args.get('from', None, type=int)
+    count = request.args.get('count',None, type=int)
 
-    try:
-        at = int(at)
-    except:
-        pass
-
-    try:
-        count = int(count)
-    except:
-        pass 
-    
-    try:
-        start = int(start)
-    except:
-        pass
-    
     if(identifier is not None):
         path = ".//*[@id='{}'] | .//*[@name='{}']".format(identifier,identifier)
 
-    response = Response(variable_instance.mtconnect.sample(path,start,count), mimetype='text/xml')
-    return response
-
-
+    response = variable_instance.mtconnect.sample(path,start,count)
+    return Response(response.get_xml(),status=response.get_status(), mimetype='text/xml')
