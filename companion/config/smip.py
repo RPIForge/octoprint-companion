ba35ee19015f0ad75c24a350077591ea53c02492
Shu_Yang <yangshuinhust@gmail.com>
2022-08-05 16:09:51 -0400

    removed authentifiation info

--- companion/config/smip.py
+++ companion/config/smip.py
@@ -1,11 +1,11 @@
 smip = {
     "verbose": 0,
-    "authenticator": "ShuYang",
-    "password": "Admin123456",
-    "name": "yangs18",
-    "role": "rpi_ro_group",
-    "url": "https://rpi.cesmii.net/graphql",
-    "bearer_token": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoicnBpX3JvX2dyb3VwIiwiZXhwIjoxNjQyNzM4Njc1LCJ1c2VyX25hbWUiOiJ5YW5nczE4IiwiYXV0aGVudGljYXRvciI6InJwaS1jZXNtaWkiLCJhdXRoZW50aWNhdGlvbl9pZCI6IjE5NTYxIiwiaWF0IjoxNjQyNzM2ODc0LCJhdWQiOiJwb3N0Z3JhcGhpbGUiLCJpc3MiOiJwb3N0Z3JhcGhpbGUifQ.wT_2jHMZg9CZA--O0V3IvmdPBazXG6qie5zNMIvlC9A",
+    "authenticator": "<Info from https://xxx.cesmii.net/developer/graphql/authentication-management>",
+    "password": "Info from https://xxx.cesmii.net/developer/graphql/authentication-management>",
+    "name": "<username on https://xxx.cesmii.net>",
+    "role": "<Info from https://uci.cesmii.net/developer/graphql/authentication-management>",
+    "url": "https://xxx.cesmii.net/graphql",
+    "bearer_token": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2FuZGJveF9ncm91cCIsImV4cCI6MTYzNzQzMjQ2NiwidXNlcl9uYW1lIjoia29yYW1iYXRoIiwiYXV0aGVudGljYXRvciI6ImNlc21paXNhbmRwcGsiLCJhdXRoZW50aWNhdGlvbl9pZCI6IjI5NCIsImlhdCI6MTYzNzM0NjA2NSwiYXVkIjoicG9zdGdyYXBoaWxlIiwiaXNzIjoicG9zdGdyYXBoaWxlIn0.Jen8cdz0Nvrr4GYHl8aJxIKRIXzvpbY8w7tpofchkwo",
     "tagids": [24410,24413],
     "startTimeOffset": 10000,
     "endTimeOffset": 0
